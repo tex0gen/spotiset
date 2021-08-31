0fb1f6d2831db28236826de2ba139100e6bf3c91
Steve <tex0gen@gmail.com>
2021-08-31 12:39:45 +0100

    Added footer links

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const port = 4000
 
 app.get('/:trackID/:trackURL', cors(), async (req, res) => {
   await fetch("https://www.1001tracklists.com/tracklist/" + req.params.trackID + "/" + req.params.trackURL, {
-  // await fetch("https://www.1001tracklists.com/tracklist/2l7s91n9/pendulum-triple-j-friday-mix-2021-05-07.html", {
     'Content-Type': 'text/html',
     headers: {
       "User-Agent": "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
@@ -36,9 +35,3 @@ app.get('/:trackID/:trackURL', cors(), async (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
 })
-
-// https://www.1001tracklists.com/tracklist/256tgj0k/hybrid-minds-pendulum-kiss-nights-2021-06-21.html
-// https://www.1001tracklists.com/tracklist/256tgj0k/hybrid-minds-&-pendulum-kiss-nights-2021-06-21.html
-
-// https://www.1001tracklists.com/tracklist/2l7s91n9/pendulum-triple-j-friday-mix-2021-05-07.html
-// https://www.1001tracklists.com/tracklist/2l7s91n9/pendulum-triple-j-friday-mix-2021-05-07.html
